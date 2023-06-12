8001d6096634e2e1a6bf485bf8edafbc3f357dcc
razorbanana <98734086+razorbanana@users.noreply.github.com>
2023-06-12 11:03:12 +0300

    feat: trying validation

--- models/person.js
+++ models/person.js
@@ -14,8 +14,23 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    maxLength: 12,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{4,9}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  },
 })
 
 const Person = mongoose.model('Person', personSchema)
--- mongo_persons.js
+++ mongo_persons.js
@@ -27,6 +27,11 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    }else if(error.name === 'ValidationError'){
+        console.log(`validation error ${error.message}`)
+        return response.status(400).json({ error: error.message })
+    }else{
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
@@ -59,14 +64,9 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
   
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-  
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, { name, number } , { new: true, runValidators: true, context: 'query' })
       .then(updatedPerson => {
         response.json(updatedPerson)
       })
