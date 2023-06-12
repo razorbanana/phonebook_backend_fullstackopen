516852aa29b06cbb7918e6804d779a4d63d6492f
razorbanana <98734086+razorbanana@users.noreply.github.com>
2023-06-12 12:58:12 +0300

    feat: forgot to renew mongo_persons with logs

--- mongo_persons.js
+++ mongo_persons.js
@@ -23,14 +23,16 @@ app.use(morgan(function (tokens, req, res) {
 app.use(cors())
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+    console.error(`errorHandler is called with ${error.message}`)
 
     if (error.name === 'CastError') {
+        console.log(`error.name = CastError`)
         return response.status(400).send({ error: 'malformatted id' })
     }else if(error.name === 'ValidationError'){
-        console.log(`validation error ${error.message}`)
+        console.log(`error.name = ValidationError`)
         return response.status(400).json({ error: error.message })
     }else{
+        console.log(`error.name = else`)
         return response.status(400).json({ error: error.message })
     }
 
@@ -41,10 +43,12 @@ const Person = require('./models/person')
 
 
 app.get('/api', (request, response) => {
+    console.log(`get /api`)
     response.send("go /persons for persons list")
 })
 
 app.get('/api/persons', (request, response) => {
+    console.log(`get /api/persons`)
     Person.find({}).then(persons => {
         response.json(persons)
     })
@@ -53,6 +57,7 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
+    console.log(`get /api/persons/:id`)
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person)
@@ -64,6 +69,7 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
+    console.log(`put /api/persons/:id`)
     const { name, number } = request.body
   
     Person.findByIdAndUpdate(request.params.id, { name, number } , { new: true, runValidators: true, context: 'query' })
@@ -74,6 +80,7 @@ app.put('/api/persons/:id', (request, response, next) => {
   })
 
 app.get('/info', (request, response) => {  
+    console.log(`get /info`)
     Person.find({}).then(persons => {
         let date = new Date(Date.now()).toUTCString()
         response.send(`<p>Phonebook has info about ${persons.length} people</p><p>${date}</p>`)
@@ -82,6 +89,7 @@ app.get('/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
+    console.log(`delete /api/persons/:id`)
     const id = Number(request.params.id)
     Person.findByIdAndDelete(request.params.id).then(res=>{
         response.status(204).end()
@@ -89,14 +97,9 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
+    console.log(`post /api/persons`)
     const body = request.body
     console.log(request.body)
-
-    if (!body.name || !body.number) {
-        response.status(400).json({
-            error: 'content missing'
-        })
-    }
 
     const person = new Person({
         name: body.name,
